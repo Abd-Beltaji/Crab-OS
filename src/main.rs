0ca9306841a4f0fc4fd262d395465b98ff23b59c
Abd-Beltaji <abd.elbeltaji@gmail.com>
2023-01-10 22:55:32 +0200

    A simple `Hello world` Kernel 👋
    We kick start our journey with building our kernel with a very simple Hello world example. This is done by using the VGA buffer in the memory (Located at 0xb8000) .

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,22 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello from Crab OS!";
 
 #[no_mangle]
 pub extern "C" fn _start() ->! {
+    let vga_buffer = 0xb8000 as *mut u8 ;
+    // A very minamialistic/simple version of our kernel!
+    // Simple Hello world writing into the VGA buffer.
+    for(i, &byte) in HELLO.iter().enumerate() {
+        unsafe { // Needed an unsafe block since we are updating memory location with our raw pointers,
+                 // which violates the memory safety of rust :(
+        // TODO: Create a VGA buffer abstraction, in order to minimize the usage of unsafe blocks.
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
  loop{}
 }
 
